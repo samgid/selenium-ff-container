e89e9c67ec0f075ae66c2156374ead23db855738
arikl <Michael19>
2016-06-09 10:08:14 +0300

    Adding selenium firefox node

--- entry_point.sh
+++ entry_point.sh
@@ -42,7 +42,7 @@ env | cut -f 1 -d "=" | sort > asroot
            -hub http://$HUB_PORT_4444_TCP_ADDR:$HUB_PORT_4444_TCP_PORT/grid/register \
            ${REMOTE_HOST_PARAM} \
            -nodeConfig /opt/selenium/config.json \
-           ${SE_OPTS} \
+           ${SE_OPTS}
   xvfb-run -n $SERVERNUM --server-args="-screen 0 $GEOMETRY -ac +extension RANDR" \
   java ${JAVA_OPTS} -cp /opt/selenium/selenium-video-node-1.9.jar:/opt/selenium/selenium-server-standalone-2.53.0.jar org.openqa.grid.selenium.GridLauncher -servlets com.aimmac23.node.servlet.VideoRecordingControlServlet -proxy com.aimmac23.hub.proxy.VideoProxy\
     -role wd \
